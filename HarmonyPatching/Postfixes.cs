ba20873e159d735b0069293fd61ad3d2583c495f
Aviroen <aviroen@gmail.com>
2024-10-01 09:06:55 -0700

    thankye atra/chu/pathos/wren for handholding me on the short circuiting &&

--- HarmonyPatching/Postfixes.cs
+++ HarmonyPatching/Postfixes.cs
@@ -10,9 +10,9 @@ namespace RoenCore.HarmonyPatching;
 public class Postfixes
 {
     public static Harmony PostfixesHarmony { get; set; } = null!;
-    public static bool IsInitialized;
+    internal static bool IsInitialized;
 
-    public static void Initialize(IManifest manifest)
+    internal static void Initialize(IManifest manifest)
     {
         IsInitialized = true;
         PostfixesHarmony = new Harmony($"{manifest.UniqueID}_Postfixes");
@@ -24,26 +24,26 @@ public class Postfixes
     {
         NPC npcSpouse = Game1.player.getSpouse();
         bool isMarriedOrRoommates = Game1.player.isMarriedOrRoommates();
-        if (npcSpouse != null && npcSpouse.GetData().CustomFields.TryGetValue("Aviroen.GSQBaby", out string customString))
+        if (npcSpouse != null && npcSpouse.GetData().CustomFields.TryGetValue("Aviroen.GSQBaby", out string? customString))
         {
-            bool.TryParse(customString, result: out bool stringActivated);
-            if (stringActivated == true)
+            if (bool.TryParse(customString, result: out bool stringActivated) && stringActivated)
             {
                 if (isMarriedOrRoommates)
                 {
                     bool? flag = npcSpouse?.canGetPregnant();
-                    if (flag.HasValue && flag.GetValueOrDefault() && Game1.player.currentLocation == Game1.getLocationFromName(Game1.player.homeLocation) && GameStateQuery.CheckConditions(npcSpouse.GetData()?.SpouseWantsChildren))
+                    if (flag.HasValue && flag.GetValueOrDefault() && Game1.player.currentLocation == Game1.getLocationFromName(Game1.player.homeLocation.Value) && GameStateQuery.CheckConditions(npcSpouse?.GetData()?.SpouseWantsChildren))
                     {
                         __result = new QuestionEvent(1);
                     }
                 }
                 if (isMarriedOrRoommates && Game1.player.team.GetSpouse(Game1.player.UniqueMultiplayerID).HasValue && Game1.player.GetSpouseFriendship().NextBirthingDate == null)
                 {
-                    long spouseID = Game1.player.team.GetSpouse(Game1.player.UniqueMultiplayerID).Value;
-                    if (Game1.otherFarmers.TryGetValue(spouseID, out var farmerSpouse))
+                    long? spouseID = Game1.player.team.GetSpouse(Game1.player.UniqueMultiplayerID);
+                    if (spouseID != null)
                     {
+                        Game1.otherFarmers.TryGetValue((long)spouseID, out var farmerSpouse);
                         Farmer spouse = farmerSpouse;
-                        if (spouse.currentLocation == Game1.player.currentLocation && (spouse.currentLocation == Game1.getLocationFromName(spouse.homeLocation) || spouse.currentLocation == Game1.getLocationFromName(Game1.player.homeLocation)) && playersCanGetPregnantHere(spouse.currentLocation as FarmHouse))
+                        if (spouse.currentLocation == Game1.player.currentLocation && (spouse.currentLocation == Game1.getLocationFromName(spouse.homeLocation.Value) || spouse.currentLocation == Game1.getLocationFromName(Game1.player.homeLocation.Value)) && Utility.playersCanGetPregnantHere(spouse.currentLocation as FarmHouse))
                         {
                             __result = new QuestionEvent(3);
                         }
@@ -53,6 +53,7 @@ public class Postfixes
         }
     }
     //NOTE TO SELF, READD "UTILITY" IN FRONT OF PLAYERSCANGETPRENGANTHERE AND YEET THE SECOND FUNCTION FOR 1.6.9
+    /*
     private static bool playersCanGetPregnantHere(FarmHouse farmHouse)
     {
         List<Child> kids = farmHouse.getChildren();
@@ -70,4 +71,23 @@ public class Postfixes
         }
         return false;
     }
+     * 	public static bool isGreenRainDay()
+	{
+		return Utility.isGreenRainDay(Game1.dayOfMonth, Game1.season);
+	}
+
+	/// <summary>Get whether there's green rain scheduled on the given day.</summary>
+	/// <param name="day">The day of month to check.</param>
+	/// <param name="season">The season key to check.</param>
+	public static bool isGreenRainDay(int day, Season season)
+	{
+		if (season == Season.Summer)
+		{
+			Random r = Utility.CreateRandom(Game1.year * 777, Game1.uniqueIDForThisGame);
+			int[] possible_days = new int[8] { 5, 6, 7, 14, 15, 16, 18, 23 };
+			return day == r.ChooseFrom(possible_days);
+		}
+		return false;
+	}
+     */
 }
