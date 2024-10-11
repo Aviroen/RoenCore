81878c2090dd6ab00fb930b3fc77ea2de8ee3355
Aviroen <aviroen@gmail.com>
2024-10-11 05:58:20 -0700

    1.0.2 - Thanks to Selph/bblueberry/Button for dealing with my logic-less stupidity

--- HarmonyPatching/Prefixes.cs
+++ HarmonyPatching/Prefixes.cs
@@ -22,43 +22,6 @@ public class Prefixes
         return true;
     }
     /*
-     * var data = building.GetData();
-if (data.CustomFields is not null && data.CustomFields.TryGetValue("Aviroen.AtraAntiCrow", out string? customString))
-  {
-    if (bool.TryParse(customString, result: out bool stringActivated) && stringActivated)
-    {
-      return false;
-    }
-}
-     * data.CustomFields is Dictionary<string, string> customFields && customFields.ContainsKey(thekey)
-     * if (building.GetData() is BuildingData data && data.CustomFields?.TryGetValue("Aviroen.AtraAntiCrow", out string? customString))
-    public static bool Prefix(ref Farm __instance)
-    {
-        
-        if (!__instance.buildings.Any(Game1.buildingData.CustomFields.Value == "Aviroen.AtraAntiCrow"))
-        {
-            __instance = new Farm();
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-    
-    iterate list of buildings on the farm
-        public static bool Prefix(Farm __instance)
-    {
-        return !__instance.buildings.Any(static building => building.buildingType.Value == "Aviroen.AtraAntiCrow");
-    }
-    
-    public static void Scarecrow()
-    {
-        Building buildingData = new Building.GetData();
-        if (building.GetData().CustomFields.TryGetValue("Aviroen.GSQBaby", out string? customString))
-        {
-
-        }
-    }
+     
     */
 }
